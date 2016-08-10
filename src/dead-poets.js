3b6c51f0b5d382db7f1fde6f656ec649eb723160
Heathcliff <dennis.nathan@gmail.com>
2016-08-10 09:26:33 -0600

    Linted all JS files, removed comments from html

--- src/dead-poets.js
+++ src/dead-poets.js
@@ -9,41 +9,40 @@ $('#inputWord').on('submit', function(event) {
       $('#synonyms').text('');
       synonyms.forEach(function(word) {
         $('#synonyms').append('<li>' + word + '</li>');
-      })
+      });
     });
   } else {
-    $('.betterWords').css('visibility', 'visible');
-    $('.betterWords h2').text('Sorry, your input must be only one word in length.')
+    $('.betterWords h2').css('visibility', 'visible');
+    $('.betterWords h2').text('Sorry, your input must be only one word in length.');
   }
 });
-
 
 function getAjaxData (word) {
   return new Promise(function(resolve, reject) {
     $.ajax ({
-    url: 'http://words.bighugelabs.com/api/2/ba781178cf379a7f5ea7f60910ce5521/' + word + '/json',
-    method: 'GET',
-    dataType: 'json'
-  }).done(function(data) {
-    resolve(processData(data));
-  }).fail(function(error) {
-    console.log(error);
+      url: 'http://words.bighugelabs.com/api/2/ba781178cf379a7f5ea7f60910ce5521/' + word + '/json',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data) {
+      resolve(processData(data));
+    }).fail(function(error) {
+      console.log(error);
+    });
   });
-})
 }
 
 function processData(data) {
   for (var pos in data) {
     partsOfSpeech.push(pos);
   }
-    var randomPos = partsOfSpeech[randomIndex(partsOfSpeech)];
-    var synonyms = data[randomPos].syn;
-    partsOfSpeech = [];
-    return synonyms;
+  var randomPos = partsOfSpeech[randomIndex(partsOfSpeech)];
+  var synonyms = data[randomPos].syn;
+  partsOfSpeech = [];
+  return synonyms;
 }
 
 function randomIndex (array) {
-    return Math.floor(Math.random() * (array.length));
+  return Math.floor(Math.random() * (array.length));
 }
 
 //Form validation
--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
+/*jshint esversion: 6 */
 const WORDSTOIGNORE = ['it', 'if', 'then', 'the', 'and', 'but', 'or', 'so', 'is', 'my', 'this', 'here', 'a', 'by', 'any', 'would', 'with', 'how', 'as', 'to', 'of', 'got', 'i', 'there', 'on', 'your', 'for', 'they','are','all'];
 
 var partsOfSpeech = [];
 
 $('#inputSentence').on('submit', function(event) {
-  event.preventDefault();
-  var sentence = $('textarea[name=sentence]').val().replace(/\./g, '').split(' ');
-  var newSentence = [];
+    event.preventDefault();
+    var sentence = $('textarea[name=sentence]').val().replace(/\./g, '').split(' ');
+    var newSentence = [];
     sentence.forEach(function(word) {
-      if (WORDSTOIGNORE.indexOf(word.toLowerCase()) > -1) {
-        newSentence.push(Promise.resolve(word));
-      } else {
-        newSentence.push(getAjaxData(word.toLowerCase()));
+        if (WORDSTOIGNORE.indexOf(word.toLowerCase()) > -1) {
+          newSentence.push(Promise.resolve(word));
+        } else {
+          newSentence.push(getAjaxData(word.toLowerCase()));
         }
-      })
+      });
     Promise.all(newSentence).then(function(combinationOfWords) {
       combinationOfWords = combinationOfWords.join(' ') + '.';
       combinationOfWords = combinationOfWords.charAt(0).toUpperCase() + combinationOfWords.slice(1);
@@ -20,46 +21,46 @@ $('#inputSentence').on('submit', function(event) {
       $('.answer').css('visibility', 'visible');
       $('#lastSentence').css('visibility', 'hidden');
       localStorage.setItem('lastSentence', combinationOfWords);
-    })
-  })
+    });
+  });
 
-  $('#getLastSentence').on('submit', function(event) {
-    event.preventDefault();
-    var last = localStorage.getItem('lastSentence');
-    $('#lastSentence').text('Your last Tribbianified sentence was: ' + last);
-    $('#lastSentence').css('visibility', 'visible');
-    $('.answer').css('visibility', 'hidden');
-  })
+$('#getLastSentence').on('submit', function(event) {
+  event.preventDefault();
+  var last = localStorage.getItem('lastSentence');
+  $('#lastSentence').text('Your last Tribbianified sentence was: ' + last);
+  $('#lastSentence').css('visibility', 'visible');
+  $('.answer').css('visibility', 'hidden');
+});
 
 function getAjaxData (word) {
   return new Promise(function(resolve, reject) {
     $.ajax ({
-    url: 'http://words.bighugelabs.com/api/2/ba781178cf379a7f5ea7f60910ce5521/' + word + '/json',
-    method: 'GET',
-    dataType: 'json'
-  }).done(function(data) {
-    console.log(data);
-    resolve(processData(data));
-  }).fail(function(error) {
-    console.log(error);
+      url: 'http://words.bighugelabs.com/api/2/ba781178cf379a7f5ea7f60910ce5521/' + word + '/json',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data) {
+      console.log(data);
+      resolve(processData(data));
+    }).fail(function(error) {
+      console.log(error);
+    });
   });
-})
 }
 
 function processData(data) {
   for (var pos in data) {
     partsOfSpeech.push(pos);
   }
-    var randomPos = partsOfSpeech[randomIndex(partsOfSpeech)];
-    var randomWord = randomIndex(data[randomPos].syn);
-    var newWord = data[randomPos].syn[randomWord];
-    partsOfSpeech = [];
-    return newWord;
+  var randomPos = partsOfSpeech[randomIndex(partsOfSpeech)];
+  var randomWord = randomIndex(data[randomPos].syn);
+  var newWord = data[randomPos].syn[randomWord];
+  partsOfSpeech = [];
+  return newWord;
 }
 
 function randomIndex (array) {
-    console.log(array);
-    return Math.floor(Math.random() * (array.length));
+  console.log(array);
+  return Math.floor(Math.random() * (array.length));
 }
 
 $('#texter').on('submit', function(event) {
@@ -70,7 +71,7 @@ $('#texter').on('submit', function(event) {
   var message = new TextMessage(phone, text);
   console.log('message', message);
   sendTextMessage(message);
-})
+});
 
 function TextMessage (number, message) {
   this.number = number;
@@ -84,12 +85,12 @@ function sendTextMessage(object) {
       data: object
     }).done(function(info) {
       if (info.success === true) {
-        $('#successOrFail').text('Sent!')
+        $('#successOrFail').text('Sent!');
       } else {
         $('#successOrFail').text('Something went wrong, please try again');
       }
     }).fail(function(error) {
       console.log(error);
-    })
-  })
+    });
+  });
 }
