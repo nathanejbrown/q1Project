039eec63c5e392cebf208f44eeb14707d5bbd2f4
Heathcliff <dennis.nathan@gmail.com>
2016-08-13 10:54:22 -0600

    Fixed glitch that caused the last sentence button to not work after viewing favorites.

---
+++ src/scripts/form-validation.js
@@ -0,0 +1,21 @@
+function checkForSuccess(input) {
+  $('#successOrFail').empty();
+  if (input.success === true) {
+    $('#successOrFail').text('Sent!');
+  } else {
+    $('#successOrFail').text('Something went wrong, please try again');
+  }
+  $('#successOrFail').css('visibility', 'visible');
+}
+
+function isPhoneNumber(number) {
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  if (number.length !== 10) {
+    return false;
+  }
+  for (x = 0; x < 10; x++) {
+    if (numbers.indexOf(number[x]) === -1)
+    return false;
+  }
+  return true;
+}
